18ab0d00460d23cd5c653d85b25f6228a9917b39
Abhi-Badhlan <78141603+Abhi-Badhlan@users.noreply.github.com>
2021-04-21 23:29:02 +0530

    stage 3
    Added Comman Navbar to every Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
-import { Route, Switch } from 'react-router';
+import { Redirect, Route, Switch } from 'react-router';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
+import Navbar from './component/Navbar'
 import Home from'./component/Home'
 import About from './component/About'
 import Contact from './component/Contact'
@@ -10,11 +12,13 @@ import Service from './component/Service'
 function App() {
   return (
     <>
+    <Navbar />
     <Switch>
       <Route exact path='/' component={Home} />
       <Route path='/about' component={About} />
       <Route path='/contact' component={Contact} />
       <Route path='/service' component={Service} />
+      <Redirect to='/' />
     </Switch>
 
     </>
