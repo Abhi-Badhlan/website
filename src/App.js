27c841695499b19dcf27077198496efa474c8adc
Abhi-Badhlan <78141603+Abhi-Badhlan@users.noreply.github.com>
2021-04-21 22:11:09 +0530

    Stage2
    Added Service Files and used the Switch to add routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react'
+import { Route, Switch } from 'react-router';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import Home from'./component/Home'
+import About from './component/About'
+import Contact from './component/Contact'
+import Service from './component/Service'
 
 
 function App() {
   return (
     <>
-    <Home />
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route path='/about' component={About} />
+      <Route path='/contact' component={Contact} />
+      <Route path='/service' component={Service} />
+    </Switch>
+
     </>
 
 
