e12a30f4e3c0589e14ed09aeb81a18ab16763840
Abhi-Badhlan <78141603+Abhi-Badhlan@users.noreply.github.com>
2021-04-22 22:03:15 +0530

    NavBar css
    Added Navbar and Some Styling

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Home=() =>{
   return (
     <>
-    <h1>Welcome to home</h1>
+  
     </>
 
 
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
 
+
 const Navbar=()=>{
     return(
         <>
@@ -11,25 +12,25 @@ const Navbar=()=>{
      
 
 
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light bg-white">
   <div className="container-fluid">
-    <NavLink className="navbar-brand" to="/abc">Navbar</NavLink>
+    <NavLink className="navbar-brand" exact to="/">AB-Technical</NavLink>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
+      <li className="nav-item">
+          <NavLink activeClassName='menu_active' className="nav-link" exact to="/">Home</NavLink>
         </li>
         <li className="nav-item">
-          <NavLink className="nav-link" to="/service">Service</NavLink>
+          <NavLink activeClassName='menu_active' className="nav-link" exact to="/service">Service</NavLink>
         </li>
         <li className="nav-item">
-          <NavLink className="nav-link" to="/about">About</NavLink>
+          <NavLink activeClassName='menu_active' className="nav-link" exact to="/about">About</NavLink>
         </li>
         <li className="nav-item">
-          <NavLink className="nav-link" to="/contact">Contact</NavLink>
+          <NavLink activeClassName='menu_active' className="nav-link" exact to="/contact">Contact</NavLink>
         </li>
        
       </ul>
